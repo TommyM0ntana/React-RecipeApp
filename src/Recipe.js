d1d8b4ebd18cc275697e11a3f3ed8a24bc8c3bb2
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-03-18 19:52:55 +0100

    add API and Recipe component

---
+++ src/Recipe.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipe = () => {
+  return (
+    <div>
+      <h1>title</h1>
+      <p>Calories</p>
+      <img src='' alt='' />
+    </div>
+  );
+};
+export default Recipe;
